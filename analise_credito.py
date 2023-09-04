dc4a188046002b8a4e039a23c91a3eca0d476440
caiotg <caiotgoncalves@gmail.com>
2023-09-04 16:10:56 -0300

    código analise de dados de credito

--- analise_credito.py
+++ analise_credito.py
@@ -1,12 +1,11 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
-import seaborn as sns
+
 
 
 def analise_credito(dadosBancarios, dicionarioColunasDummies):
